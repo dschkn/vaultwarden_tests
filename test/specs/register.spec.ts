52b4ce174f4b3c080bb73ba71382775eb7588b2c
dschkn <dmitrii.schtschukin@gmail.com>
2024-07-18 20:40:35 +0300

    added register.spec.ts and wrong.password.spec.ts

--- test/specs/register.spec.ts
+++ test/specs/register.spec.ts
@@ -1,4 +1,4 @@
-import Login from "../pageobjects/login.ts";
+/*import Login from "../pageobjects/login.ts";
 import Registration from "../pageobjects/register.ts";
 import { expect } from "chai";
 
@@ -6,21 +6,17 @@ describe("Page elements test", () => {
   let RegistrationPage = new Registration();
   const currentTime = new Date().getTime();
   const generatedEmail = `user${currentTime}@test.com`;
-  const generatedPassword = RegistrationPage.generateRandomPassword();
+  const generatedPassword = RegistrationPage.fixedPassword();
+
   it("clicks the Create account button", async () => {
     let RegistrationPage = new Registration();
     await RegistrationPage.open();
     await RegistrationPage.createAccount.click();
-    await browser.waitUntil(
-      async () => {
-        return await RegistrationPage.fillInForm.isDisplayed();
-      },
-      {
-        timeout: 5000,
-        timeoutMsg: "The element did not appear within 5 seconds",
-      }
-    );
+    await browser.waitUntil(async () => {
+      return (await RegistrationPage.fillInForm).waitForDisplayed();
+    });
   });
+
   it("should enter a randomly generated email based on current time into the email input field", async () => {
     try {
       let RegistrationPage = new Registration();
@@ -32,12 +28,14 @@ describe("Page elements test", () => {
       throw error;
     }
   });
+
   it("should fill the registration form with a random name", async () => {
     let registrationPage = new Registration();
     const randomName = `User${Math.floor(Math.random() * 10000)}`;
     await registrationPage.nameInput.setValue(randomName);
     expect(await registrationPage.nameInput.getValue()).to.equal(randomName);
   });
+
   it("should fill the password input with a random password", async () => {
     let RegistrationPage = new Registration();
     await RegistrationPage.masterPasswordInput.setValue(generatedPassword);
@@ -47,22 +45,21 @@ describe("Page elements test", () => {
     await RegistrationPage.confirmMasterPasswordInput.setValue(
       generatedPassword
     );
-    expect(await RegistrationPage.masterPasswordInput.getValue()).to.equal(
-      generatedPassword
-    );
     expect(
       await RegistrationPage.confirmMasterPasswordInput.getValue()
     ).to.equal(generatedPassword);
   });
+
   it("should fill the hint input with 'HINT'", async () => {
     const RegistrationPage = new Registration();
     await RegistrationPage.hintInput.setValue("HINT");
     expect(await RegistrationPage.hintInput.getValue()).to.equal("HINT");
   });
+
   it("should click the Create account button", async () => {
     const registrationPage = new Registration();
     await registrationPage.createAccountButton.click();
-    await registrationPage.toastContainer.waitForDisplayed({ timeout: 5000 });
+    await registrationPage.toastContainer.waitForDisplayed();
     const toasterText = await registrationPage.toastContainer.getText();
     expect(toasterText).to.include("Your new account has been created");
     await browser.pause(2000);
@@ -78,11 +75,12 @@ describe("Page elements test", () => {
     await LoginPage.masterPasswordInput.setValue(generatedPassword);
     await LoginPage.loginWithMasterPasswordButton.click();
   });
+
   it("should check that login to your account was successful", async () => {
     let LoginPage = new Login();
     const currentUrl = await browser.getUrl();
     expect(currentUrl).to.include(`vault`);
-    await LoginPage.navigationBar.waitForDisplayed({ timeout: 5000 });
+    await LoginPage.headerTitle.waitForDisplayed();
     await browser.pause(2000);
   });
 });
---
+++ test/specs/wrong.password.spec.ts
@@ -0,0 +1,80 @@
+/*import Registration from "../pageobjects/register.ts";
+import { expect } from "chai";
+
+describe("Page elements test", () => {
+  let RegistrationPage = new Registration();
+  const currentTime = new Date().getTime();
+  const generatedEmail = `user${currentTime}@test.com`;
+  const wrongEmail = `user${currentTime}test.com`;
+  const generatedPassword = RegistrationPage.fixedPassword();
+  const shortPassword = RegistrationPage.fixedShortPassword();
+
+  it("clicks the Create account button", async () => {
+    let RegistrationPage = new Registration();
+    await RegistrationPage.open();
+    await RegistrationPage.createAccount.click();
+    await RegistrationPage.fillInForm.waitForDisplayed();
+  });
+
+  it("checks whether the information entered in the email field is actually an email", async () => {
+    let RegistrationPage = new Registration();
+    await RegistrationPage.emailInput.setValue(wrongEmail);
+    await RegistrationPage.emailBitError();
+  });
+
+  it("should enter a randomly generated email based on current time into the email input field", async () => {
+    let RegistrationPage = new Registration();
+    await RegistrationPage.emailInput.setValue(generatedEmail);
+  });
+
+  it("checks if password length is correct", async () => {
+    let RegistrationPage = new Registration();
+    await RegistrationPage.masterPasswordInput.setValue(shortPassword);
+    await RegistrationPage.confirmMasterPasswordInput.setValue(
+      "any_another_password123456789"
+    );
+    await RegistrationPage.masterPasswordBitError(); //ловит сообщение что пасворд недостаточно длинный
+    await RegistrationPage.masterPasswordInput.clearValue();
+    await RegistrationPage.masterPasswordInput.setValue(generatedPassword);
+  });
+
+  it("checks the sameness of the entered passwords", async () => {
+    let RegistrationPage = new Registration();
+    await RegistrationPage.confirmMasterPasswordBitError(); // ловит сообщение что пасворды не одинаковые
+  });
+
+  it("should fill the hint input with 'HINT'", async () => {
+    const RegistrationPage = new Registration();
+    await RegistrationPage.hintInput.setValue("HINT");
+  });
+
+  it("checks final error prompt", async () => {
+    const RegistrationPage = new Registration();
+    await RegistrationPage.createAccountButton.click();
+    await RegistrationPage.errorSummary.waitForDisplayed(); // ловит всплывающее сообщение внизу что вообще есть ошибки
+  });
+  /*
+  it("checks the popup window is displayed", async () => {
+    const RegistrationPage = new Registration();
+    await RegistrationPage.masterPasswordInput.setValue("111111111111");
+    await RegistrationPage.confirmMasterPasswordInput.setValue("111111111111");
+    await RegistrationPage.createAccountButton.click();
+    await browser.pause(5000);
+    await RegistrationPage.dialogContainer.waitForDisplayed();
+    await RegistrationPage.noButton.click();
+  });
+
+
+  /*
+  it("checks the inability to create account with short password.", async () => {
+    const RegistrationPage = new Registration();
+    await RegistrationPage.masterPasswordInput.setValue(shortPassword);
+    await RegistrationPage.confirmMasterPasswordInput.setValue(shortPassword);
+    const initialUrl = await browser.getUrl();
+    await RegistrationPage.createAccountButton.click();
+    await browser.pause(2000);
+    await RegistrationPage.errorSummary.waitForDisplayed();
+    const newUrl = await browser.getUrl();
+    expect(newUrl).equal(initialUrl);
+  });*/
+});
